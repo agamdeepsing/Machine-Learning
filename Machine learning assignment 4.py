d731cef2c9f12c7f2ef500d58aab7622a2cef46e
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-26 03:03:51 +0530

    Add files via upload

---
+++ Machine learning assignment 4.py
@@ -0,0 +1,86 @@
+
+# ques 1
+data collection ,data preparation, selecting model with best accuracy, training testing, evaluating, parameter tuning , prediction
+
+
+# Ques 2
+numerical data, timeseries data , categoriacal data.
+
+
+...numerical data further divided into two 
+
+1. continuous - height weight salary(has any value witin the range)
+2. discrete - total students(it has distinct values)
+
+
+...categorical - it represents position of each player. it takes numericla value 
+we can use any number and associate that number to that color.
+
+
+Time series data:  is a sequence of numbers collected at regular intervals over some period of time.
+it is important in the field of finance.
+
+
+
+# Ques 3
+1.numeric are also known as quantitaive or continuous variables which represents numerical values that can be counted.ex - heght weight age income temp. it is of two types
+discrete or comtinuous. it allows us to do numeric opereations such as div add sub mul and many more.
+
+while categorical is known as qualitative and nominal variables. it ususally represents labels categories qualituies. ex male or female colour, qualification. 
+Categorical attributes are represented by labels or categories rather than numerical values. it does not allow mathematical operations. it is visualised over bar chart pie chart.
+
+
+2.Selection of features On the other hand, you just choose a subset of the original feature set without modifying the data.
+
+Dimensionality reduction is often defined as selecting a new representation that can explain most but not all of the variation in your data, maintaining the important information but lowering the amount of information required to express it.
+
+
+
+
+# Ques 4
+the histogram is the graphical representation of distribution of a dataset.it shows the freq count of data of different interval, which is also
+called as bins . it includes x asis, y axis, bins, bar height, bar width.it is used to visualize the shape and central tendenct (mean median mode).
+it is most commonly used in data analysis and statistics.
+
+
+scatter plot is used to visualize the relationship between two variables. it is represented with dots i.e comapring x axis and ay axis values and plot it with dots.
+They are valuable for understanding the direction, strength, and nature of relationships between variables.
+
+
+# Ques 5
+Investigating data is necessary to gain a deeper understanding of the underlying patterns, relationships, and insights that the data holds.
+1. detecting errors and outliers understanding distributions  and patterns supporting decision making.
+
+Regarding the discrepancy in exploring qualitative and quantitative data, Qualitative data exploration,
+Quantitative data exploration,
+
+
+# Ques6
+1. uniform histogram 
+2. symmetric histogram
+3. probaility histogram
+4. bimodal histogram
+
+bins are used to give spacing on the x axis. bins are created to simplify the data and make it manageable. it is also important to consider the range and distribution of data. 
+
+
+# Ques 7
+we can use z-score for dealing with outliers.
+identify and understand the outliers, keep the outliers , remove the outliers, transform the data.                                 
+
+
+# QUes 8
+there are three types of central inclination measures i.e mean median mode.The mean and median can vary significantly in certain datasets due to the presence of outliers. 
+when there is outlier they have an impact on mean while no impact on median.
+
+
+# Ques 9
+it uses dots to represents the values for two different values,position of each dot on the horizontal and vertical axis indicates values for an individual data point. 
+So this visualization gives us the idea of bivariate relationship.
+
+Scatter plot can also help finding outliers as outliers can be visualized at farther distance than regular data.
+
+
+# Ques 10
+it is used to analyze the relationship between two variables.it gives us the way to summarize and analyze by creating contigencytable.
+it allows us to make informed decisions influencing the relationship.
