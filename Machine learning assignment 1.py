cf15882ed87f53fcea3e8ce2d3f171f85ecd6d94
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-26 03:38:14 +0530

    Add files via upload

---
+++ Machine learning assignment 1.py
@@ -0,0 +1,94 @@
+
+# Ques 1
+ML is a subfield of AI that helps software programmes to grow increasingly accurate at predicting events without being specifically designed to do so. Machine learning algorithms estimate new output values by using past data as input.
+
+
+# Ques 2
+Image Recognition: One of the most popular applications of machine learning is image recognition. It is used to recognise items, people, places, digital pictures, and so on. Automatic buddy tagging recommendation is a prominent use of picture recognition and facial identification.
+
+Speech Recognition: When we use Google, we have the option of searching by voice, which falls under speech recognition and is a prominent Machine Learning application. Speech recognition, often known as speech to text or computer-based speech recognition, is the process of turning spoken commands into text. Machine learning techniques are currently widely employed in a variety of voice recognition applications. Google Assistant, Siri, Cortana, and Alexa use speech recognition technologies to respond to spoken commands.
+
+
+credit card fraud detection - Machine learning for credit card fraud detection entails gathering transaction data, preprocessing and engineering important aspects, and using algorithms to discover patterns of fraudulent behaviour. Machine learning models are trained on historical data to detect abnormalities and estimate the possibility of fraud in real-time transactions, allowing for prompt intervention and fraud prevention.
+
+
+mental health detection - Machine learning for mental health detection analyses data such as text, user input, and facial recognition in photographs to uncover patterns associated to mental health disorders. Although it can aid in early identification and assistance, ethical concerns, privacy protection, and human oversight are required to assure responsible and accurate outcomes.
+
+
+# Ques 3
+You divide the data that contains known response variable values into two halves. The training set is used to train the algorithm, and the learned model is then used on the test set to predict the known response variable values.
+
+
+# Ques 4
+The two most common supervised learning tasks are Regression and Classification.
+
+
+# Ques 5
+Four common Unsupervised Tasks included Clustering, Visualization, Dimensionality Reduction, and Association Rule Learning.
+
+
+# QUes 6
+Reinforced Learning is the greatest Machine Learning method for allowing a robot to walk on new terrains, as the robot can learn from the terrain's response to optimise itself.
+
+
+# Ques 7
+The Best Algorithm to Segment Customers into different groups is either Supervised Learning (if the groups have known labels) or Unsupervised Learning (if there are no group labels).
+
+
+# Ques 8
+Spam detection is a Supervised Machine Learning problem because the labels are known (spam or no spam).
+
+
+# Ques 9
+Online learning system is a learning system in which the machine learns continously, as data is given in small streams continuously.
+
+
+# Ques 10
+An out-of-core learning system is one that can manage data that is too large to fit within your computer's memory. It feeds data in little pieces using an online learning method.
+
+
+
+# Ques 11
+Learning algorithm that relies on a similarity measure to make predictions is Instance Based Algorithm.
+
+
+# Ques 12
+A model parameter controls how a model predicts given a new instance. A model typically contains more than one parameter (for example, the slope of a linear model). A hyperparameter is a learning algorithm parameter, not a model parameter.
+
+
+
+# Ques 13
+The model-based learning method seeks the optimal parameter value in a model that will produce the best outcomes for new instances. To establish what the parameter value must be in order to minimise the function, we frequently use a cost function or something similar. The model predicts using the new instance's value and the parameters in its function.
+
+
+# Ques 14
+Overfitting the Data (using a model too complicated)
+Underfitting the data (using a simple model)
+Lacking in Data
+Non Representative Data.
+
+
+# Ques 15
+If the model fails to perform well on fresh instances, it has overfitted on the training data. We have three options for dealing with this:
+
+Obtain further information
+Implement a more basic model.
+
+Remove any outliers or noise from the current data collection.
+
+
+
+# Ques 16
+Test set is a set to test your model (fit using training data) to see how it performs.Test set is necessary to determine how good (or bad) a model performs.
+
+
+# Ques 17
+Validation set is a set used to compare between different training models.
+
+
+# Ques 18
+Cross-validation is a technique for examining models without the requirement for a separate validation set. It is preferable over the set used for validation because we may avoid splitting up an element of the training set for generating the validation set, and having more data is always important.
+
+
+# Ques 19
+If you adjust hyperparameters using test sets, the model may not perform well on out-of-sample data since it is tuned only for that set.
